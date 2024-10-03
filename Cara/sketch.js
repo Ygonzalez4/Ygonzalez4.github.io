7e57b5c4055d2f6b6bba18d0bf01e195a9be2a61
Ygonzalez4 <ygonzale4@iespompeufabra.cat>
2024-10-03 14:01:05 +0200

    Create sketch.js

---
+++ Cara/sketch.js
@@ -0,0 +1,24 @@
+ 
+
+function setup() {
+  createCanvas(400,600);
+}
+
+function draw() {
+  background(0);
+  fill(108,59,42);
+  ellipse(200,200,300,300)
+  fill (255,223,196)
+  ellipse(200,300,320,100)
+  fill (255,223,196)
+  ellipse (200,300,300,400);
+  fill (70, 202, 242);
+  ellipse (140,200,50,50);
+  ellipse (260,200,50,50)
+  fill (255,0,0)
+  arc (200,400,200,100,0,PI);
+ fill(0,0,0)
+  triangle(200,260,190,300,210,300)
+    fill (79, 128, 156)
+  ellipse (200,660,500,300);
+ }
